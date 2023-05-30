2c2e77563e605ac30bfd450de2daa79883e08b3a
Rafael Pirotto <rafael.pirotto@moove-it.com>
2023-05-30 14:14:04 -0300

    Add env setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 import { AuthenticationModule } from './authentication/authentication.module';
 import { SubscriptionsModule } from './subscriptions/subscriptions.module';
@@ -15,13 +16,28 @@ import { Movie } from './movies/entities/movie.entity';
     // here we import all of our modules
     AuthenticationModule,
     UsersModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'movie-subscriber.sqlite',
-      entities: [User, Subscription, Movie],
-      // TODO: cambiar esto en production:
-      synchronize: true,
+    // We could use TypeOrmModule.forRoot({…}) and enter the configuration options
+    // but because we are loading environment variable from the .env file we need to import ConfigModule
+    // and inject ConfigService first.
+    TypeOrmModule.forRootAsync({
+      // source: https://blog.devgenius.io/setting-up-nestjs-with-postgresql-ac2cce9045fe
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: +configService.get<number>('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [User, Subscription, Movie],
+        synchronize:
+          configService.get<string>('ENV').toLowerCase() !== 'production',
+      }),
+      inject: [ConfigService],
     }),
+    // The setting isGlobal: true will automatically import ConfigModule to all other modules,
+    // which means you do not need to import it in Users.module.ts file
+    ConfigModule.forRoot({ isGlobal: true }),
     SubscriptionsModule,
     MoviesModule,
   ],
