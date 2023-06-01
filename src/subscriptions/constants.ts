37d7ceed45ecc73798a33867ec8ba7b98e91646e
Rafael Pirotto <rafael.pirotto@moove-it.com>
2023-06-01 14:22:54 -0300

    Refactor

--- src/subscriptions/constants.ts
+++ src/subscriptions/constants.ts
@@ -1,5 +1,7 @@
 export const ACTIVE_SUBSCRIPTION = 'Subscription is already active';
-export const NO_ACTIVE_SUBSCRIPTION = 'No active subscription found';
+export const NO_SUBSCRIPTION_FOUND = 'No active subscription found';
+export const INACTIVE_SUBSCRIPTION =
+  'Subscription already exists but is inactive because tickets were already available';
 export const buildUrl = (id: string) =>
   `https://api.movie.com.uy/api/content/detail?contentId=${id}`;
 export const MOVIE_DOES_NOT_EXIST_IN_MC_DB =
