7e9c9994230aa158f094e0940f1ec2f9ed7dfc55
Rafael Pirotto <rafael.pirotto@moove-it.com>
2023-06-16 17:02:30 -0300

    Add oppenheimer bs as tickets

--- src/cronjobs/cronjobs.service.ts
+++ src/cronjobs/cronjobs.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { parse } from 'node-html-parser';
+import axios from 'axios';
 
 import { MoviesService } from 'src/movies/movies.service';
 import { NotificationsService } from 'src/notifications/notifications.service';
 import { PushoverDevice } from 'src/notifications/types';
 import { Subscription } from 'src/subscriptions/entities/subscription.entity';
 import { SubscriptionsService } from 'src/subscriptions/subscriptions.service';
+import { Priority } from 'src/subscriptions/types';
 
 export type AvailableSubscription = Subscription & {
   isMovieAvailable: boolean;
@@ -17,7 +20,7 @@ export class CronjobsService {
     private subscriptionsService: SubscriptionsService,
     private moviesService: MoviesService,
     private notificationsService: NotificationsService
-  ) { }
+  ) {}
   private readonly logger = new Logger(CronjobsService.name);
 
   private checkTicketsAvailability = (activeSubscriptions: Subscription[]) => {
@@ -51,6 +54,43 @@ export class CronjobsService {
       this.logger.error(
         'There was an error trying to notify the users',
         availableSubscriptions
+      );
+    }
+  }
+  @Cron(CronExpression.EVERY_5_MINUTES)
+  async oppenheimerTicketsAreAvailable() {
+    const { data: webpage } = await axios.get(
+      'https://www.voyalcine.net/showcase/'
+    );
+    const parsedWebpage = parse(JSON.stringify(webpage));
+    const result = parsedWebpage.querySelectorAll(
+      '.content-section , .name , img'
+    );
+    const filteredResult = result.filter((r) => r.rawTagName !== 'img');
+    const filteredResultLength = filteredResult.length;
+    const finalResult = filteredResult.slice(
+      filteredResultLength / 2,
+      filteredResultLength
+    );
+    const finalRes = finalResult.map((r) =>
+      r.childNodes[0].rawText.toLowerCase()
+    );
+    const ticketsAreAvailable = !!finalRes.find((movie) =>
+      movie.includes('oppenheimer')
+    );
+    const now = new Date().toLocaleString('en-GB', {
+      timeZone: 'America/Montevideo',
+    });
+    this.logger.log(
+      `Checked for oppenheimer tickets in Buenos Aires at ${now}`
+    );
+    if (ticketsAreAvailable) {
+      this.logger.log(`Movie array is ${finalRes}`);
+      await this.notificationsService.send(
+        'Tickets for Oppenheimer are ready in Buenos Aires',
+        'Tickets for Oppenheimer are ready in Buenos Aires',
+        Priority.EMERGENCY,
+        PushoverDevice.IPHONE_RAFA
       );
     }
   }
